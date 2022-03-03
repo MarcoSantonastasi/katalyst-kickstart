4fa01de7de84cefeef6d64c03838736064a77a18
Marco Santonastasi <44085372+MarcoSantonastasi@users.noreply.github.com>
2022-03-03 16:01:37 +0000

    isolated python dir

--- ruby/spec/service/user_service_spec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative '../../model/user'
-require_relative '../../repository/user_repository'
-require_relative '../../service/user_service'
-
-RSpec.describe UserService do
-end
